130db9f3e5722f0074c7563451f35570bc39d7cb
Omarqui Aybar <omarqui1996@gmail.com>
2020-07-02 18:02:08 -0400

    modifica readme file

--- Readme.mk
+++ Readme.mk
@@ -1,6 +1,8 @@
 # Smart Pecera Webapi
-[] Crear tabla para Pecera con {id, nombre}
+[x] Crear tabla para Pecera con {id, nombre}
 [] Crear tabla caracteriscasPercera con {id, idPecera, nombre, dato}
+[] Crear endPoints para tabla Pecera
+[] Crear endPoints para tabla caracteriscasPercera
 [] Agregar idPecera a tablas existentes
 [] Agregar idPecera a endPoints existentes
 [] Persistencia en DB
