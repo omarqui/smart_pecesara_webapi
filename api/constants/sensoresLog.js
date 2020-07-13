aa7777c32146dea29c0dce421ff7570395dc833e
Omarqui Aybar <omarqui1996@gmail.com>
2020-07-13 08:53:50 -0400

    limpia datos de prueba

--- api/constants/sensoresLog.js
+++ api/constants/sensoresLog.js
@@ -6,15 +6,6 @@ let data = [{
     temperatura: "23",
     PH: "5",
     GH: "7",
-},
-{
-    _id: 2,
-    idPecera: 1,
-    createdOn: "2020-11-08T7:03:00",
-    nivel: "20",
-    temperatura: "25",
-    PH: "8",
-    GH: "6",
 }];
 
 module.exports = data;
