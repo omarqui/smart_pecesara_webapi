a9f1fe3f8179d4c35795ed6dc5151b2123a4361b
omarqui <omarquiaybar@gmail.com>
2020-07-24 23:17:41 -0400

    sustituye id de pecera por idManual

--- api/controllers/peceraController.js
+++ api/controllers/peceraController.js
@@ -1,62 +1,63 @@
 const moment = require('moment');
-let dbService;
+const dbService = require("../models/pecera");
+
+const $project = {
+    _id: 0,
+    _id: "$idManual",
+    nombre: 1
+};
 
 class PeceraController {
-    constructor(service){
-        if(service)
-            dbService = service;
-        else
-            dbService = require("../models/pecera");        
-    }
-
     async getPecera(req, res) {
-        const list = await dbService.find();
+        const list = await dbService.aggregate([{$project}]);
         res.json(list);
     }
 
     async getPeceraById(req, res) {
-        const searched = await dbService.findById(req.params.id)
+        const searched = await dbService.aggregate([{
+            $match: { "idManual": req.params.id }
+        },{$project}]);
 
-        res.json(searched);
+    res.json(searched);
+}
+
+async updatePecera(req, res) {
+    await dbService.findAndUpdate({ idManual: req.params.id}, { nombre: req.body.nombre });
+
+    res.send({});
+}
+
+async savePeceraFromBody(req, res) {
+    let newPecera = req.body;
+
+    const data = await dbService.find();
+    if (data.length > 0) {
+        const lastId = data[data.length - 1]
+        newPecera.idManual = lastId.idManual ? Number(lastId.idManual) + 1 : 1;
+    } else {
+        newPecera.idManual = 1
     }
 
-    async updatePecera(req, res) {
-        await dbService.findByIdAndUpdate(req.params.id, { nombre: req.body.nombre });
+    const pecera = new dbService(newPecera);
+    await pecera.save();
 
-        res.send({});
-    }
+    const newConfig = {
+        idPecera: pecera.idManual,
+        fechaInternaReloj: moment().format(),
+        proximaFechaMantenimiento: moment().format("YYYY-MM-DD"),
+        horarioComida1: moment().format("HH:mm"),
+        horarioComida2: moment().add(1, "hour").format("HH:mm"),
+        docificacionManual: 0,
+        pendienteActualizar: 1,
+        estadoActuadores: 0
+    };
 
-    async savePeceraFromBody (req, res) {
-        let newPecera = req.body;
-        
-        const data = await dbService.find();
-        if (data.length > 0){
-            const lastId = data[data.length - 1]
-            newPecera.idManual = lastId.idManual ? lastId.idManual + 1 : 1;
-        } else {
-            newPecera.idManual = 1 
-        }
-        
-        const pecera = new dbService(newPecera);
-        await pecera.save();
+    const configModel = require("../models/configuracion");
+    const config = new configModel(newConfig);
+    config.save();
 
-        const newConfig = {            
-            idPecera: pecera._id,
-            fechaInternaReloj: moment().format(),
-            proximaFechaMantenimiento: moment().format("YYYY-MM-DD"),
-            horarioComida1: moment().format("HH:mm"),
-            horarioComida2: moment().add(1, "hour").format("HH:mm"),
-            docificacionManual: 0,
-            pendienteActualizar: 1,
-            estadoActuadores: 0
-        };
-
-        const configModel = require("../models/configuracion");
-        const config = new configModel(newConfig);
-        config.save();
-
-        res.send({});
-    }
+    res.send({});
+}
 }
 
 module.exports = PeceraController;
